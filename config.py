6d81642220e1bf8b4746cd6ddae4c33c0637b574
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-08 20:12:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID = 12345678  # replace with your actual API ID
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-OWNER_ID = 123456789  # replace with your Telegram user ID
+API_ID = 28744454  # replace with your actual API ID
+API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7"
+BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg"
+OWNER_ID = 6266529037  # replace with your Telegram user ID
 
 # Webhook settings
-WEBHOOK_URL = "https://your-koyeb-or-render-url"
+WEBHOOK_URL = "https://"
 PORT = 8080
 
 # MongoDB
-MONGO_URI = "your_mongodb_uri"
+MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority"
 
 # Start message images
 START_IMAGES = [
